3f4b6dc083a569b41c65f4e89364c59affc037bc
Nagwa <nagwa.ahmed419@gmail.com>
2024-12-01 21:02:59 +0200

    app file

---
+++ app.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+nums=np.random.rand(30)
+
+print(nums)
+
+from joblib import load
+
+model_rf=load('rf.joblib')
+
+
+from flask import Flask
+app=Flask(__name__)
+
+@app.route('/')
+def hello ():
+    return 'Hello world'
